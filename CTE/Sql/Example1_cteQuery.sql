addbf8b56281d639053c8ad11d732d1175176ebe
Dean Mincic <dean.mincic@enett.com>
2020-04-03 07:42:36 +1100

    added Example1 CTE query

---
+++ CTE/Sql/Example1_cteQuery.sql
@@ -0,0 +1,23 @@
+
+
+    
+	DECLARE @EmpId INT = 3;
+
+	;WITH cte1 AS
+	(
+		SELECT EmpId
+			  ,MgrId
+		FROM dbo.Employees
+		WHERE MgrId = @EmpId
+ 
+		UNION ALL
+  
+		SELECT e.EmpId
+			  ,e.MgrId
+		FROM dbo.Employees e
+		INNER JOIN cte1 c
+			ON e.MgrId = c.EmpId
+	)
+	SELECT cte1.EmpId
+		  ,cte1.MgrId
+	FROM cte1;
