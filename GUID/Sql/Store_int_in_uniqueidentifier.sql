56f88e32643634ff512cb8ee4250364395e3bc73
Dean Mincic <dean.mincic@enett.com>
2020-08-22 08:25:21 +1000

    change bigint to numeric

--- GUID/Sql/Store_int_in_uniqueidentifier.sql
+++ GUID/Sql/Store_int_in_uniqueidentifier.sql
@@ -1,16 +1,21 @@
-DECLARE @aBigintNo BIGINT =   123456789987654321;     
+DECLARE @BigNumeric DECIMAL(28,0) = 1234567898765321234567898765;   
 /*
-For the demonstration we can use, say the maximum positive bigint number
-SET @aBigintNo =  -1* (POWER(CAST (-2 AS BIGINT),63) +1)
+For the demonstration I used a positive integer that requires more than 8bytes of space 
 */
 
 DECLARE @binary16 BINARY(16)
        ,@guid UNIQUEIDENTIFIER
 
-SET @binary16 = @aBigintNo
-SET @guid = @binary16 --implicit conversion bigint to uniqueidentifier is not permitted
+SET @binary16 = @BigNumeric
+SET @guid = @binary16 --implicit conversion from a numeric to uniqueidentifier is not permitted
 
-SELECT [decimal] = @aBigintNo
+SELECT [decimal] = @BigNumeric
       ,[binary(16)] = @binary16
       ,[GUID] = @guid;
+
+--to convert back to numberic
+/*
+SELECT CONVERT(DECIMAL(28,0), @binary16) --bin -> numberic
+SELECT CONVERT(DECIMAL(28,0),CONVERT(BINARY(16), @guid)) --guid -> bin -> numeric
+*/
 GO
