da23c731f2847cf29c7e8abb7887761d1679243f
Dean Mincic <dean.mincic@enett.com>
2020-08-25 10:47:14 +1000

    python, generate guid

---
+++ GUID/ClientCode/GenerateGUID.py
@@ -0,0 +1,25 @@
+
+import uuid,pyodbc
+
+# Create connection
+cnxn = pyodbc.connect(driver=r"{SQL Server}" \
+                     ,server=r".\SQL2019" \
+                     ,database=r"TEST2019DB" \
+                     ,Trusted_Connection=r"yes" \
+                     ,autocommit=True) ## default is False -> set implicit_transactions on
+
+cursor = cnxn.cursor()
+
+#create a guid value
+ui = uuid.uuid4()
+
+#define tsql command and params
+sql = "EXECUTE dbo.TestGUIDinsert @guid = ?"
+params = ui
+
+#execute stored proc 
+cursor.execute(sql,params) ##RPC call - parameterised batch request
+
+##print(ui)
+cnxn.close()
+
