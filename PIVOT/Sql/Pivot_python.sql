a682644bce3da37675004f84592cb80682a31563
Dean Mincic <dean.mincic@enett.com>
2021-02-21 12:18:39 +1100

    pivot with python

---
+++ PIVOT/Sql/Pivot_python.sql
@@ -0,0 +1,46 @@
+
+/*
+sp_execute_external_script   
+    @language = N'language' ,   
+    @script = N'script',  
+    @input_data_1 = ] 'input_data_1'   
+    [ , @input_data_1_name = ] N'input_data_1_name' ]   
+    [ , @output_data_1_name = 'output_data_1_name' ]  
+    [ , @parallel = 0 | 1 ]  
+    [ , @params = ] N'@parameter_name data_type [ OUT | OUTPUT ] [ ,...n ]'  
+    [ , @parameter1 = ] 'value1' [ OUT | OUTPUT ] [ ,...n ]  
+    [ WITH <execute_option> ]  
+[;] 
+*/
+
+EXEC sys.sp_execute_external_script
+         @language = N'Python'
+        ,@script   = N'
+
+dfpivot = df.pivot_table(index = ["Shipcountry"], \
+                         columns =["OrderYear"], \
+                         values = ["Freight"], \
+                         aggfunc={"Freight":sum}, \
+                         fill_value=None).reset_index(level="Shipcountry")
+
+## dfpivot =dfpivot.reset_index(level="Shipcountry") ##we can reshape the data frame in a separate statement.
+print(dfpivot)
+
+dfpivot_out = dfpivot' 
+
+        ,@input_data_1 =N'SELECT   Orderid
+                                  ,Custid
+                                  ,Orderdate
+                                  ,OrderYear
+                                  ,Shipperid
+                                  ,OrderValue = CAST(OrderValue AS FLOAT)
+                                  ,Freight = CAST(Freight AS FLOAT)
+                                  ,Shipname
+                                  ,Shipaddress
+                                  ,Shipcity
+                                  ,Shipregion
+                                  ,Shipcountry 
+                        FROM dbo.Orders_testPivot;'
+        ,@input_data_1_name =N'df' -- the default value is InputDataSet
+        ,@output_data_1_name =N'dfpivot_out'
+    WITH RESULT SETS  ((Shipcountry NVARCHAR(15),[2018] MONEY, [2019]  MONEY,[2020] MONEY));
