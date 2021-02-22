0176e83ad65e341db4087c3fcaf720680e209668
Dean Mincic <dean.mincic@enett.com>
2021-02-22 17:22:09 +1100

    add multi agg multi group pivot

---
+++ PIVOT/Sql/MultiAggMultiGroupPivot_python.sql
@@ -0,0 +1,44 @@
+
+/*
+sp_execute_external_script   
+    @language = N'language' ,   
+    @script = N'script',  
+    @input_data_1 = ] 'input_data_1'   
+    [ , @input_data_1_name = ] N'input_data_1_name' ]   
+    [ , @output_data_1_name = 'output_data_1_name' ]  
+    [ , @parallel = 0 | 1 ]  
+    [ , @params = ] N'@parameter_name data_type [ OUT | OUTPUT ] [ ,...n ]'  
+    [ , @parameter1 = ] 'value1' [ OUT | OUTPUT ] [ ,...n ]  
+    [ WITH <execute_option> ]  
+[;] 
+*/
+
+EXEC sys.sp_execute_external_script
+         @language = N'Python'
+        ,@script   = N'
+import numpy as np
+dfpivot_out = df.pivot_table(index = ["Shipcountry","Shipregion"], \
+                              columns =["OrderYear"], \
+                              values = ["Freight","OrderValue"], \
+                              aggfunc={"Freight":sum,"OrderValue":np.average}).reset_index(level=["Shipcountry","Shipregion"])
+print(dfpivot_out)
+' 
+        ,@input_data_1 =N'SELECT   Orderid
+                                  ,Custid
+                                  ,Orderdate
+                                  ,OrderYear
+                                  ,Shipperid
+                                  ,OrderValue = CAST(OrderValue AS FLOAT)
+                                  ,Freight = CAST(Freight AS FLOAT)
+                                  ,Shipname
+                                  ,Shipaddress
+                                  ,Shipcity
+                                  ,Shipregion
+                                  ,Shipcountry 
+                        FROM dbo.Orders_testPivot;'
+        ,@input_data_1_name =N'df' -- the default value is InputDataSet
+        ,@output_data_1_name =N'dfpivot_out'
+   WITH RESULT SETS  ((Shipcountry NVARCHAR(15),Shipregion NVARCHAR(15),[2018] FLOAT, [2019] FLOAT,[2020] FLOAT
+                                              ,[Avg Order Value(2018)] FLOAT
+                                              ,[Avg Order Value(2019)] FLOAT
+                                              ,[Avg Order Value(2020)] FLOAT))
