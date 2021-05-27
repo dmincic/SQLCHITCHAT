e90594f46328018554fde5cd74ff497b2fdae96b
Dean Mincic <dean.mincic@enett.com>
2021-05-28 09:02:07 +1000

    add namespaces

--- COLLATION/Code/WinVsSQLCollation.ps1
+++ COLLATION/Code/WinVsSQLCollation.ps1
@@ -1,6 +1,9 @@
 ## Create a simple connection to Sql Server using .NET Framework Data Provider for Sql Server
 
-$sqlConn = New-Object System.Data.SqlClient.SqlConnection;
+using namespace System.Data.SqlClient;
+using namespace System.Data;
+
+$sqlConn = New-Object SqlConnection;
 
 $sqlConn.ConnectionString = @('Data Source=tcp:<server\instance_name>;' `
                              +'Initial Catalog=testCollation;' `
@@ -14,7 +17,7 @@ $sqlConn.Open();
     $sqlCmd = $sqlConn.CreateCommand()
     $sqlCmd.CommandText = "SELECT * FROM dbo.Employees WHERE LastName = @lname";
 
-    $sqlParam = New-Object System.Data.SqlClient.SqlParameter("@lname",[System.Data.sqlDbType]::NVarChar,50);
+    $sqlParam = New-Object SqlParameter("@lname", [sqlDbType]::NVarChar,50);
 
     $sqlCmd.Parameters.ADD($sqlParam).Value="Goldberg";
     ##execute the parameterised batch request
